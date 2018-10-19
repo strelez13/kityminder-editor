2b1885a8748ab6a32145876f2bdbe3ca42931c50
ACTom <i@actom.me>
2018-10-20 00:48:14 +0800

    make dist for cherry-pick

--- dist/kityminder.editor.min.js
+++ dist/kityminder.editor.min.js
@@ -1,6 +1,6 @@
 /*!
  * ====================================================
- * kityminder-editor - v1.0.64 - 2018-10-18
+ * kityminder-editor - v1.0.64 - 2018-10-20
  * https://github.com/fex-team/kityminder-editor
  * GitHub: https://github.com/fex-team/kityminder-editor 
  * Copyright (c) 2018 ; Licensed 
